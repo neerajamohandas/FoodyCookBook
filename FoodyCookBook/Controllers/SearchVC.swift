b0f7f59533fea5958d959786694864529c31de92
Neeraja Mohandas <neerajamohandas@gmail.com>
2022-02-08 22:12:39 +0530

    search bar partially completed

--- FoodyCookBook/Controllers/SearchVC.swift
+++ FoodyCookBook/Controllers/SearchVC.swift
@@ -38,9 +38,18 @@ extension SearchVC: UISearchBarDelegate {
         if let text = searchBar.text {
         let api = Constants.API.searchFood.rawValue + text
         NetworkService.shared.getDataFromServer(url: api) { _data, _error in
-            if let data = _data {
-                print(data)
+            if _error == nil {
+                if let data = _data {
+                    let foods = data.value(forKey: "meals") as! NSArray
+                    for i in 0...(foods.count - 1) {
+                        self.results.append(foods[i] as! Food)
+                    }
+                }
+            }else {
+                print("No item found")
             }
+            
+            
         }
     }
     }
